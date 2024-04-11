c304bd59bfb6faa751620f2c2073ba042ca3ee50
vishwajeet <vishwajeet01798@gmail.com>
2024-04-12 03:31:59 +0530

    footer link active

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,10 +13,16 @@ function Footer() {
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
             <div className="flex space-x-4">
-              <FaFacebook size={24} />
+            <a href="https://www.facebook.com/" target="_blank">
+              <FaFacebook size={24} className="text-2xl cursor-pointer" />
+              </a>
               <FaTwitter size={24} />
+              <a href="https://www.instagram.com/vishwajeet_verma1/" target="_blank">
               <FaInstagram size={24} />
+              </a>
+              <a href="https://www.linkedin.com/in/vishwajeet-verma-52622b203/" target="_blank">
               <FaLinkedinIn size={24} />
+              </a>
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
               <p className="text-sm">
